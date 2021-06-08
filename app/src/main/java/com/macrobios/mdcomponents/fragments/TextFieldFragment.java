97fbb6c0842bfe0f2d6602fe721454ff72aba68c
Jonathan Sanchez <jonulisanpar@gmail.com>
2021-06-08 14:41:38 -0500

    Material Desing, TextField: Eventos Personalizados

--- app/src/main/java/com/macrobios/mdcomponents/fragments/TextFieldFragment.java
+++ app/src/main/java/com/macrobios/mdcomponents/fragments/TextFieldFragment.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -32,6 +34,33 @@ public class TextFieldFragment extends Fragment {
                              Bundle savedInstanceState) {
         binding = FragmentTextFieldBinding.inflate(inflater, container, false);
         View view = binding.getRoot();
+
+        //Eventos de endIcon del textField
+        binding.tilCapitalLetter.setEndIconOnClickListener(v -> {
+            if(binding.etCapitalLetter.getText() != null){
+                String contentSTR = binding.etCapitalLetter.getText().toString();
+                binding.etCapitalLetter.setText(contentSTR.toUpperCase());
+            }
+        });
+
+        binding.etPrice.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(!s.toString().isEmpty() && Integer.valueOf(s.toString()) < 5){
+                    binding.etPrice.setError(getString(R.string.error_price_min));
+                }
+            }
+        });
 
         // Inflate the layout for this fragment
         return view;
