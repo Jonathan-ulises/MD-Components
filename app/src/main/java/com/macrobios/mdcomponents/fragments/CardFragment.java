173ae6035e33446373b0fb3d4a301feb6e8c6e2c
Jonathan Sanchez <jonulisanpar@gmail.com>
2021-06-10 11:29:24 -0500

    Card View, avanzado, Glide y RaintBar: Material Design

--- app/src/main/java/com/macrobios/mdcomponents/fragments/CardFragment.java
+++ app/src/main/java/com/macrobios/mdcomponents/fragments/CardFragment.java
@@ -8,6 +8,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.RequestOptions;
 import com.macrobios.mdcomponents.R;
 import com.macrobios.mdcomponents.databinding.FragmentCardBinding;
 import com.macrobios.mdcomponents.databinding.FragmentCheckBoxBinding;
@@ -33,6 +36,13 @@ public class CardFragment extends Fragment {
                              Bundle savedInstanceState) {
         binding = FragmentCardBinding.inflate(inflater, container, false);
         View view = binding.getRoot();
+
+        RequestOptions options = new RequestOptions().diskCacheStrategy(DiskCacheStrategy.ALL).centerCrop();
+
+        Glide.with(this)
+                .load("https://sevilla.abc.es/gurme/wp-content/uploads/sites/24/2020/04/ramen-pollo1920-960x540.jpg")
+                .apply(options)
+                .into(binding.imgCardLarge);
 
         // Inflate the layout for this fragment
         return view;
