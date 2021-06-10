8e90ac44435c3c76f03e7c389899e97737f9b711
Jonathan Sanchez <jonulisanpar@gmail.com>
2021-06-10 14:29:57 -0500

    Dialog, FullScreen(diseño): Material Components

---
+++ app/src/main/java/com/macrobios/mdcomponents/fragments/FullScreenDialogFragment.java
@@ -0,0 +1,23 @@
+package com.macrobios.mdcomponents.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.macrobios.mdcomponents.R;
+
+public class FullScreenDialogFragment extends Fragment {
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_full_screen_dialog, container, false);
+    }
+}
