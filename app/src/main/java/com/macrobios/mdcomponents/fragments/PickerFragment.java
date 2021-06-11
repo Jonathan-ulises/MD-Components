46d3ff32605feabfd60a3ffb76cfa2a11d1d39f3
Jonathan Sanchez <jonulisanpar@gmail.com>
2021-06-11 10:48:46 -0500

    Pickers, FullScreen, eventos :Material Components

--- app/src/main/java/com/macrobios/mdcomponents/fragments/PickerFragment.java
+++ app/src/main/java/com/macrobios/mdcomponents/fragments/PickerFragment.java
@@ -8,12 +8,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.datepicker.CalendarConstraints;
 import com.google.android.material.datepicker.MaterialDatePicker;
+import com.google.android.material.snackbar.Snackbar;
 import com.macrobios.mdcomponents.R;
 import com.macrobios.mdcomponents.databinding.FragmentAppBarBinding;
 import com.macrobios.mdcomponents.databinding.FragmentPickerBinding;
 import com.macrobios.mdcomponents.utils.Commons;
 import com.macrobios.mdcomponents.utils.Component;
+
+import java.sql.Date;
 
 public class PickerFragment extends Fragment {
     public static final String TAG = "Picker";
@@ -34,12 +38,45 @@ public class PickerFragment extends Fragment {
         binding = FragmentPickerBinding.inflate(inflater, container, false);
         View view = binding.getRoot();
 
+
+        MaterialDatePicker.Builder<Long> builder = MaterialDatePicker.Builder.datePicker();
+        builder.setTitleText(R.string.picker_title);
+        builder.setSelection(System.currentTimeMillis());
+
         binding.btnDialog.setOnClickListener(v -> {
             //Constructor de pickers
-            MaterialDatePicker.Builder<Long> builder = MaterialDatePicker.Builder.datePicker();
-            builder.setTitleText(R.string.picker_title);
             builder.setTheme(R.style.ThemeOverlay_MaterialComponents_MaterialCalendar);
             MaterialDatePicker<?> picker = builder.build();
+
+            picker.addOnPositiveButtonClickListener(selection -> {
+                //Snackbar.make(binding.containerMain, R.string.message_action_success, Snackbar.LENGTH_LONG).show();
+                Snackbar.make(binding.containerMain, picker.getHeaderText(), Snackbar.LENGTH_LONG).show();
+            });
+            picker.addOnNegativeButtonClickListener(dialog -> {
+                Snackbar.make(binding.containerMain, R.string.dialog_negative, Snackbar.LENGTH_LONG).show();
+            });
+            picker.addOnCancelListener(dialogInterface -> {
+                Snackbar.make(binding.containerMain, R.string.dialog_cancel, Snackbar.LENGTH_LONG).show();
+            });
+
+            picker.show(getFragmentManager(), picker.toString());
+        });
+
+        binding.btnullScreen.setOnClickListener(v -> {
+            //builder.setTheme(R.style.ThemeOverlay_MaterialComponents_MaterialCalendar_Fullscreen);
+            builder.setTheme(R.style.FullScreenPicker);
+            MaterialDatePicker<?> picker = builder.build();
+
+            picker.addOnPositiveButtonClickListener(selection -> {
+                Snackbar.make(binding.containerMain, picker.getHeaderText(), Snackbar.LENGTH_LONG).show();
+            });
+            picker.addOnNegativeButtonClickListener(dialog -> {
+                Snackbar.make(binding.containerMain, R.string.dialog_negative, Snackbar.LENGTH_LONG).show();
+            });
+            picker.addOnCancelListener(dialogInterface -> {
+                Snackbar.make(binding.containerMain, R.string.dialog_cancel, Snackbar.LENGTH_LONG).show();
+            });
+
             picker.show(getFragmentManager(), picker.toString());
         });
 
